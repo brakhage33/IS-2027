0e931ae625293dfaeff8fd4ed655868871db8e5b
JoseAngelPrada <uo300078@uniovi.es>
2026-09-25 16:41:02 +0000

    Ejercicio 3 (continuación)

---
+++ sesion1-3/udp_cliente3_espera_ok.py
@@ -0,0 +1,45 @@
+import socket
+import sys
+
+# Valores por defecto
+HOST = "localhost"
+PUERTO = 9999
+contador = 1
+
+# Lectura de argumentos por línea de comandos (IP y Puerto)
+if len(sys.argv) > 1:
+    HOST = sys.argv[1]
+if len(sys.argv) > 2:
+    PUERTO = int(sys.argv[2])
+
+# Crear socket UDP
+cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+print(f"Cliente UDP listo para enviar a {HOST}:{PUERTO}")
+print("Escribe tus mensajes. Envía 'FIN' para terminar.\n")
+
+while True:
+    linea = input("> ")
+    mensaje = f"{contador}: {linea}"
+    contador+=1
+    # Condición de parada
+    if linea == "FIN":
+        break
+    
+    # Enviar datos codificados en UTF-8
+    cliente.sendto(mensaje.encode("utf-8"), (HOST, PUERTO))
+
+    cliente.settimeout(2)
+    try:
+        datos, direccion = cliente.recvfrom(1024)
+        mensaje = datos.decode("utf-8")
+        if mensaje=="OK":
+            print("Recibida confirmación")
+        else:
+            print("Recibido datagrama no esperado")
+    except socket.timeout:
+        print("ERROR. El datagrama de confirmación no llega")
+
+# Cerrar el socket al salir del bucle
+cliente.close()
+print("Cliente finalizado.")
